dc7a0c10518a441c6f43ee6f5aa44f7c24216543
olp <t.miyai0001@gmail.com>
2025-01-09 18:01:38 +0900

    ログイン機能の作成、仮登録、本登録の修正

--- app/Services/MemberService.php
+++ app/Services/MemberService.php
@@ -18,8 +18,8 @@ class MemberService
         return $this->memberRepository->findBy($field, $operator, $value);
     }
 
-    public function updateMember($name, $password, $apiToken, $postalCode, $address, $address2, $address3, $tel)
+    public function updateMember($name, $password, $token, $postalCode, $address, $address2, $address3, $tel)
     {
-        return $this->memberRepository->updateMember($name, $password, $apiToken, $postalCode, $address, $address2, $address3, $tel);
+        return $this->memberRepository->updateMember($name, $password, $token, $postalCode, $address, $address2, $address3, $tel);
     }
 }
